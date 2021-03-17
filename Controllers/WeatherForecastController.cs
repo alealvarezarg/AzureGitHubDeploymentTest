d8b52c28271070551436fe42eca58e9f880a11a9
Alejandro Alvarez Arguello <alealvarezarg@gmail.com>
2021-03-16 19:04:53 -0500

    Second commit

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -31,7 +31,7 @@ namespace WebAppAzureTest.Controllers
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Environment = "Github",
+                Environment = "Github repo",
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
